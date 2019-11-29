5a66d90a7a4ab40f7ebd8b7320e032f5ca0d4485
Matthew Honour <matthewpeterhonour@gmail.com>
2019-11-29 22:50:48 +0000

    Made a separate function for the basic get request

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Hello World')
+const express = require('express')
+const app = express()
+const port = 3000
+
+function getPizzas(req,res){
+	res.send('Getting all Pizza!')
+}
+
+app.get('/pizzas', (req,res)=>getPizzas(req,res))
+
+app.get('/*', (req,res)=>res.send('404 -> Resource not found'))
+
+app.listen(port, () => console.log(`Your 🍕 pizza API is running on port ${port}!`))
